0043bf8c9e12924c5f6af28302ba800a94eeb497
Tim Acker <tjacker31@gmail.com>
2017-05-10 12:45:50 -0400

    CSS Concatenate and Minify
    Added dependencies and code to concatenate and minify CSS files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,79 @@
 /*global window, console, $, jQuery, require, alert*/
 
 const gulp = require('gulp'),
-      concat = require('gulp-concat'),
-      uglify = require('gulp-uglify'),
+      concatCss = require('gulp-concat-css'),
+      concatJs = require('gulp-concat'),
+      minJs = require('gulp-uglify'),
+      minCss = require('gulp-csso'),
       rename = require('gulp-rename'),
-      maps = require('gulp-sourcemaps');
+      maps = require('gulp-sourcemaps'),
+      del = require('del');
+
+const sourceCss = [
+  'src/css/normalize.css',
+  'src/css/foundation.css',
+  'src/css/basics.css',
+  'src/css/menu.css',
+  'src/css/hero.css',
+  'src/css/photo-grid.css',
+  'src/css/modals.css',
+  'src/css/footer.css',
+];
+
+const sourceJs = [
+  'src/js/jquery.js',
+  'src/js/fastclick.js',
+  'src/js/foundation.js',
+  'src/js/foundation.equalizer.js',
+  'src/js/foundation.reveal.js'
+];
+
+const sourceBuild = [
+  'src/css/application.min.css',
+  'src/js/app.min.js',
+  'src/index.html',
+  'src/img/**'
+];
 
 gulp.task('concatScripts', () => {
-  return gulp.src([
-    'src/js/jquery.js',
-    'src/js/fastclick.js',
-    'src/js/foundation.js',
-    'src/js/foundation.equalizer.js',
-    'src/js/foundation.reveal.js'
-  ])
+  return gulp.src(sourceJs)
     .pipe(maps.init())
-    .pipe(concat('app.js'))
+    .pipe(concatJs('app.js'))
     .pipe(maps.write('./'))
     .pipe(gulp.dest('src/js'));
 });
 
-gulp.task('minifyScripts', ['concatScripts'], function () {
+gulp.task('concatStylesheets', () => {
+  return gulp.src(sourceCss)
+    .pipe(maps.init())
+    .pipe(concatCss('application.css'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('minifyScripts', ['concatScripts'], () => {
   return gulp.src('src/js/app.js')
-    .pipe(uglify())
+    .pipe(minJs())
     .pipe(rename('app.min.js'))
-    .pipe(gulp.dest('src/js'));
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minifyStylesheets', ['concatStylesheets'], () => {
+  return gulp.src('src/css/application.css')
+    .pipe(minCss())
+    .pipe(rename('application.min.css'))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('clean', function () {
+  del(['dist', 'src/css/application.css*', 'src/js/app*.js*']);
+});
+
+gulp.task('build', ['minifyScripts', 'minifyStylesheets'], () => {
+  return gulp.src(sourceBuild, {base: 'src'})
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['clean'], () => {
+  gulp.start('build');
 });
