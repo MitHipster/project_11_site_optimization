5925164ceaf9091b5d927e5b2cec8e977cc8a5f8
Tim Acker <tjacker31@gmail.com>
2017-05-09 23:58:32 -0400

    JS Concatenate and Minify
    Added dependencies and code to concatenate and minify javascript files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@
 /*global window, console, $, jQuery, require, alert*/
 
 const gulp = require('gulp'),
-      concat = require('gulp-concat');
+      concat = require('gulp-concat'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename'),
+      maps = require('gulp-sourcemaps');
 
 gulp.task('concatScripts', () => {
   return gulp.src([
@@ -11,5 +14,16 @@ gulp.task('concatScripts', () => {
     'src/js/foundation.js',
     'src/js/foundation.equalizer.js',
     'src/js/foundation.reveal.js'
-  ]);
+  ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('src/js'));
+});
+
+gulp.task('minifyScripts', ['concatScripts'], function () {
+  return gulp.src('src/js/app.js')
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('src/js'));
 });
